2180c76dda29aef17091f4d33974521ae4907196
Edvan-Junior <edvan.junior@novaroma.edu.br>
2025-09-14 22:48:30 +0000

    correção do app

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import requests
 
 app = Flask(__name__)
+
 API_KEY = '5ae2e3f221c38a28845f05b6b17c37432db21ed64fbb106cf2739220'
 
 def get_coords(city):
     url = f'https://api.opentripmap.com/0.1/en/places/geoname?name={city}&apikey={API_KEY}'
     response = requests.get(url).json()
-    if 'lat' in response and 'lon' in response:
-        return response['lat'], response['lon']
-    print(f"Erro ao buscar coordenadas: {response}")
-    return None, None
+    return response.get('lat'), response.get('lon')
 
 def get_places(lat, lon):
     url = f'https://api.opentripmap.com/0.1/en/places/radius?radius=2000&lon={lon}&lat={lat}&rate=2&format=json&apikey={API_KEY}'
     response = requests.get(url).json()
-    return [{'nome': p['name'], 'tipo': p.get('kinds')} for p in response]
+    return [
+        {
+            'nome': p['name'],
+            'tipo': p.get('kinds'),
+            'lat': p['point']['lat'],
+            'lon': p['point']['lon'],
+            'xid': p['xid']
+        }
+        for p in response
+    ]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/pontos', methods=['GET'])
 def pontos_turisticos():
